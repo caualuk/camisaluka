50bae85817c512651e42d8c640e84cc266780646
caualuk <cauacsl07@gmail.com>
2024-12-24 22:28:51 -0300

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Navbar/>
-=======
-      <div className="welcome">
+    <Navbar/>
+    <div className="welcome">
         <h1>Luka Store</h1>
-      </div>
->>>>>>> fabf4b9f98081dfeb77a91b6faed9263d14f4774
+    </div>
     </>
   );
 }
